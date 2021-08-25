2c1cd58224e82529cb4aa1d7621996f0dd5ac38c
Thiago Gomes Possa <thiagopossa@prefeitura.sp.gov.br>
2021-08-25 00:06:46 -0300

    Atualizada a página de total de pacientes

--- web/controllers/painel_visitas.js
+++ web/controllers/painel_visitas.js
@@ -2,9 +2,24 @@ const { DateTime } = require("luxon")
 
 module.exports = app => {
     app.route('/painel_visitas')
-    .get((req, res) => {
+    .get((req, res) => {      
+      
+      res.render('painel_visitas.ejs', { data : null})
+    })
+    .post((req, res) => { 
+      let pesquisa = req.body.pesquisa      
+      console.log(pesquisa)
+
+      // Aqui estou limitando em 20
+      // Vou optar por não fazer a paginação agora
+      // mas pra fazer eu preciso manter o valor pesquisado
+      // a pesquisa também deverá ocorrer pelo nome do acompanhante
+      // e pelo rh do paciente..
+      // mas por hora isso demandaria muito tempo e tem funcões 
+      // mais importantes no momento.
+      
       db.collection('visitas_passadas')
-        .find()
+        .find( { nome: new RegExp(pesquisa, 'i')} ).limit( 30 )
         .toArray((err, results) => {
           if (err) return console.log(err)       
 
@@ -14,10 +29,10 @@ module.exports = app => {
 
           results.forEach(p => {
             p.data = DateTime.fromISO(p.data).toLocaleString(DateTime.DATETIME_SHORT)
-          })
+          })          
 
           res.render('painel_visitas.ejs', { data: results })
         })
+        
     })
-    
   }
